bae318b8ad0aa5abd876cd8489ad3676e669ffdc
Nicholas Ocket <nicholas@aboutcoding.be>
2026-07-31 13:10:45 +0200

    configuration screen

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { ArchiveBrowser } from './features/archives/archive-browser/archive-browser';
 import { ArchiveDetail } from './features/archives/archive-detail/archive-detail';
+import { ConfigurationPage } from './features/configuration/configuration-page/configuration-page';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'archives', pathMatch: 'full' },
   { path: 'archives', component: ArchiveBrowser },
   { path: 'archives/:id', component: ArchiveDetail },
+  { path: 'config', component: ConfigurationPage },
 ];
